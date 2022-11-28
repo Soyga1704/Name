fbe083e97132c6187f7d9de3188bca4bd8962025
Soyga1704 <114419182+Soyga1704@users.noreply.github.com>
2022-11-28 16:07:10 +0300

    Create main_snake.py

---
+++ main_snake.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+import random
+import time
+
+from snake_food import Food
+from snake_snake import Snake
+from snake_game import Game
+
+game = Game()
+snake = Snake(game.green)
+food = Food(game.brown, game.screen_width, game.screen_height)
+
+game.init_and_check_for_errors()
+game.set_surface_and_title()
+
+while True:
+    snake.change_to = game.event_loop(snake.change_to)
+
+    snake.validate_direction_and_change()
+    snake.change_head_position()
+    game.score, food.food_pos = snake.snake_body_mechanism(
+        game.score, food.food_pos, game.screen_width, game.screen_height)
+    snake.draw_snake(game.play_surface, game.white)
+
+    food.draw_food(game.play_surface)
+
+    snake.check_for_boundaries(
+        game.game_over, game.screen_width, game.screen_height)
+
+    game.show_score()
+    game.refresh_screen()
