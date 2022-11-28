ae0b0977579f6889f0b96390cbf736da12aac499
MakrMary <114419268+MakrMary@users.noreply.github.com>
2022-11-28 16:46:47 +0300

    Update snake_food.py

--- snake_food.py
+++ snake_food.py
@@ -1,7 +1,6 @@
 class Food():
-    def __init__(self, food_color, screen_width, screen_height):
+    def __init__(self, screen_width, screen_height):
         '''Инит еды'''
-        self.food_color = food_color
         self.food_size_x = 10
         self.food_size_y = 10
         self.food_pos = [random.randrange(1, screen_width/10)*10,
@@ -9,7 +8,6 @@ class Food():
 
     def draw_food(self, play_surface):
         '''Отображение еды'''
-        pygame.draw.rect(
-            play_surface, self.food_color, pygame.Rect(
+        play_surface.blit(mushroomImg, (
                 self.food_pos[0], self.food_pos[1],
                 self.food_size_x, self.food_size_y))
